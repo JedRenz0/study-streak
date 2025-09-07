8fb963bc223e5a3445ec1fe244de8b7e2fcf8774
Jed Villapando <jvillapa@uoguelph.ca>
2025-09-07 17:02:44 -0400

    feat: update hello message for testing

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 def main():
-    print("Hello, Study Streak Tracker")
+    #print("Hello, Study Streak Tracker")
+    print("Hello, this is study streak tracker 2")
 
 if __name__ == "__main__":
     main()
+
